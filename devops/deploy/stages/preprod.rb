63196091993a34f03573c91ba94ed1de5280691f
Paul Jehanno <paulj@theodo.fr>
2016-01-21 15:51:49 +0100

    retour sur preprod en user ubuntu

--- devops/deploy/stages/preprod.rb
+++ devops/deploy/stages/preprod.rb
@@ -1,7 +1,7 @@
 set :branch, 'preprod'
 
 set :sudo, 'false'
-set :ssh_user, 'www-data'
+set :ssh_user, 'ubuntu'
 set :deploy_to, '/home/ubuntu/admin-myfirstsymfonyproject.com'
 
 server '62.4.23.208', user: fetch(:ssh_user), roles: %w{app}
